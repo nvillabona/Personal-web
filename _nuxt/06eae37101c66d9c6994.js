780f6974fa434ee0feaa3bb96b54e621ff4b3999
Nicolas Villabona <nvillabona97@gmail.com>
2020-04-22 12:37:09 -0500

    Add files via upload

---
+++ _nuxt/06eae37101c66d9c6994.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{201:function(t,e,n){"use strict";n.r(e);var r={},l=n(38),component=Object(l.a)(r,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"container"},[e("b-jumbotron",{attrs:{header:"🛠",lead:""}})],1)}),[],!1,null,null,null);e.default=component.exports}}]);
